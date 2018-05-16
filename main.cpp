dc28aad87c0a991bd0d0a207db3fe780c72ebddb
aers <aers00@gmail.com>
2018-05-15 22:59:32 -0700

    Version check in preload initialize

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "../skse64_loader_common/IdentifyEXE.h"
 #include "../skse64/PluginAPI.h"
 #include "../skse64_common/skse_version.h"
 #include "../skse64_common/BranchTrampoline.h"
@@ -6,6 +7,7 @@
 
 #include "config.h"
 #include "patches.h"
+#include "util.h"
 #include <cinttypes>
 
 IDebugLog	gLog;
@@ -74,8 +76,34 @@ extern "C" {
 	// preloader
 	void Initialize()
 	{
-		preloaded = true;
 		SetupLog();
+		// check version
+		// can't use the skse plugin interface for obvious reasons
+		{
+			TCHAR filename[MAX_PATH];
+			GetModuleFileName(nullptr, filename, MAX_PATH);
+
+			VS_FIXEDFILEINFO info;
+			std::string productName;
+			std::string productVersion;
+
+			if (GetFileVersion(filename, &info, &productName, &productVersion))
+			{
+				uint64_t version;
+
+				VersionStrToInt(productVersion, &version);
+
+				const uint32_t skse_internal_version = MAKE_EXE_VERSION(version >> 48, version >> 32, version >> 16);
+
+				if (skse_internal_version != RUNTIME_VERSION_1_5_39)
+				{
+					_FATALERROR("unsupported runtime version %08X", version);
+					return;
+				}
+			}
+		}	
+
+		preloaded = true;
 		LoadConfig();
 
 		_MESSAGE("plugin was preloaded, allowing preload-required features");
