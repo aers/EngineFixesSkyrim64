d802b9bd68f68bae0e5d18dee3b8bfcbf0284bc6
aers <aers00@gmail.com>
2025-08-19 08:49:29 -0700

    fix form caching correctly

--- src/patches/form_caching.h
+++ src/patches/form_caching.h
@@ -87,33 +87,31 @@ namespace Patches::FormCaching
             return result;
         }
 
-        inline SafetyHookInline g_hk_SetAt{};
+        static inline REL::Relocation<bool(std::uintptr_t a_self, RE::FormID* a_formIdPtr, RE::TESForm** a_valueFunctor, void* a_unk)> _FormScatterTable_SetAt;
 
         // the functor stores the TESForm to set as the first field
-        inline bool FormScatterTable_SetAt(std::uintptr_t a_self, RE::FormID* a_formIdPtr, RE::TESForm** a_valueFunctor)
+        inline bool FormScatterTable_SetAt(std::uintptr_t a_self, RE::FormID* a_formIdPtr, RE::TESForm** a_valueFunctor, void* a_unk)
         {
-            // don't insert forms of form id 0, there's some temporary thing going on I dont understand
-            if (*a_formIdPtr != 0) {
-                const std::uint8_t  masterId = (*a_formIdPtr & 0xFF000000) >> 24;
-                const std::uint32_t baseId = (*a_formIdPtr & 0x00FFFFFF);
+            const std::uint8_t  masterId = (*a_formIdPtr & 0xFF000000) >> 24;
+            const std::uint32_t baseId = (*a_formIdPtr & 0x00FFFFFF);
 
-                RE::TESForm* formPointer = *a_valueFunctor;
+            RE::TESForm* formPointer = *a_valueFunctor;
 
 #ifdef USE_TBB
-                HashMap::accessor a;
-                if (!g_formCache[masterId].emplace(a, baseId, formPointer)) {
-                    logger::trace("replacing an existing form in form cache"sv);
-                    a->second = formPointer;
-                }
+            HashMap::accessor a;
+            if (!g_formCache[masterId].emplace(a, baseId, formPointer)) {
+                logger::trace("replacing an existing form in form cache"sv);
+                a->second = formPointer;
+            }
 #else
-                g_formCache[masterId].try_emplace_l(baseId, [&formPointer](HashMap::value_type& v) {
+            g_formCache[masterId].try_emplace_l(baseId, [&formPointer](HashMap::value_type& v) {
                     logger::trace("replacing an existing form in form cache"sv);
                     v.second = formPointer; }, formPointer);
 #endif
 
-                TreeLodReferenceCaching::detail::CheckAndRemoveForm(baseId, formPointer);
-            }
-            return g_hk_SetAt.call<bool>(a_self, a_formIdPtr, a_valueFunctor);
+            TreeLodReferenceCaching::detail::CheckAndRemoveForm(baseId, formPointer);
+
+            return _FormScatterTable_SetAt(a_self, a_formIdPtr, a_valueFunctor, a_unk);
         }
 
         inline SafetyHookInline g_hk_ClearData;
@@ -142,35 +140,6 @@ namespace Patches::FormCaching
             g_hk_InitializeFormDataStructures.call();
         }
 
-        inline SafetyHookInline g_hk_SetFormID;
-
-        inline void TESForm_SetFormID(RE::TESForm* a_self, RE::FormID a_formID, bool a_updateFile)
-        {
-            if (a_self->GetFormID() != a_formID) {
-                if ((a_self->GetFormFlags() & RE::TESForm::RecordFlags::kTemporary) != 0) {
-                    const std::uint8_t  masterId = (a_formID & 0xFF000000) >> 24;
-                    const std::uint32_t baseId = (a_formID & 0x00FFFFFF);
-
-                    const std::uint8_t  oldMasterId = (a_self->GetFormID() & 0xFF000000) >> 24;
-                    const std::uint32_t oldBaseId = (a_self->GetFormID() & 0x00FFFFFF);
-
-                    g_formCache[oldMasterId].erase(oldBaseId);
-#ifdef USE_TBB
-                    HashMap::accessor a;
-                    if (!g_formCache[masterId].emplace(a, baseId, a_self)) {
-                        logger::info("replacing an existing form in form cache"sv);
-                        a->second = a_self;
-                    }
-#else
-                    g_formCache[masterId].try_emplace_l(baseId, [&a_self](HashMap::value_type& v) {
-                        logger::trace("replacing an existing form in form cache"sv);
-                        v.second = a_self; }, a_self);
-#endif
-                }
-            }
-            g_hk_SetFormID.call(a_self, a_formID, a_updateFile);
-        }
-
         inline void ReplaceFormMapFunctions()
         {
             REL::Relocation getForm{ REL::ID(14617) };
@@ -179,17 +148,23 @@ namespace Patches::FormCaching
             const REL::Relocation RemoveAt{ REL::ID(14710) };
             g_hk_RemoveAt = safetyhook::create_inline(RemoveAt.address(), FormMap_RemoveAt);
 
-            const REL::Relocation SetAt{ REL::ID(14688) };
-            g_hk_SetAt = safetyhook::create_inline(SetAt.address(), FormScatterTable_SetAt);
+            // there is one call that is not the form table so we will callsite hook
+            constexpr std::array todoSetAt = {
+                std::pair(14593, 0x2B0),
+                std::pair(14593, 0x301),
+                std::pair(14666, 0xFE)
+            };
+
+            for (auto& [id, offset] : todoSetAt) {
+                REL::Relocation target { REL::ID(id), offset };
+                _FormScatterTable_SetAt = target.write_call<5>(FormScatterTable_SetAt);
+            }
 
             const REL::Relocation ClearData{ REL::ID(13754) };
             g_hk_ClearData = safetyhook::create_inline(ClearData.address(), TESDataHandler_ClearData);
 
             const REL::Relocation InitializeFormDataStructures{ REL::ID(14669) };
             g_hk_InitializeFormDataStructures = safetyhook::create_inline(InitializeFormDataStructures.address(), TESForm_InitializeFormDataStructures);
-
-            const REL::Relocation SetFormID { REL::ID(14666) };
-            g_hk_SetFormID = safetyhook::create_inline(SetFormID.address(), TESForm_SetFormID);
         }
     }
 
